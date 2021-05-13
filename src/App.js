7cd41483e8c68b907dd61e059ede3ad53fc04dac
fimovol <fimovolpe@gmail.com>
2021-05-13 11:09:55 -0500

    agregando cambios no importantes

--- src/App.js
+++ src/App.js
@@ -7,14 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>esta es mi aplicacion mano respete</h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>este es un cambio insignificante</h2>
       </header>
     </div>
   );
